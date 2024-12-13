2378a463470e499be92832994ecde496e1e87cc0
spvagelis <spirou.vagelis@gmail.com>
2024-12-13 18:03:01 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TestSPM",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TestSPM",
+            targets: ["TestSPM"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TestSPM"),
+        .testTarget(
+            name: "TestSPMTests",
+            dependencies: ["TestSPM"]),
+        .binaryTarget(name: "PrintHelloSwift", path: "./Sources/PrintHelloSwift.xcframework")
+    ]
+)
---
+++ Sources/PrintHelloSwift.xcframework/ios-arm64/PrintHelloSwift.framework/Headers/PrintHelloSwift.h
@@ -0,0 +1,18 @@
+//
+//  PrintHelloSwift.h
+//  PrintHelloSwift
+//
+//  Created by Vangelis Spirou on 13/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrintHelloSwift.
+FOUNDATION_EXPORT double PrintHelloSwiftVersionNumber;
+
+//! Project version string for PrintHelloSwift.
+FOUNDATION_EXPORT const unsigned char PrintHelloSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrintHelloSwift/PublicHeader.h>
+
+
