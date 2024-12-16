08423b5ecd58c3b62cd0f7a1b5267b3ca22dffd4
spvagelis <spirou.vagelis@gmail.com>
2024-12-16 12:52:00 +0200

    - Add PrintHelloSwift zip file - Changes in the Package file

--- Package.swift
+++ Package.swift
@@ -14,12 +14,17 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TestSPM"),
+            name: "TestSPM",
+            dependencies: [
+            .target(name: "PrintHelloSwift")
+        ]),
         .testTarget(
             name: "TestSPMTests",
             dependencies: ["TestSPM"]),
-        .binaryTarget(name: "PrintHelloSwift", path: "./Sources/PrintHelloSwift.xcframework")
+        .binaryTarget(
+            name: "PrintHelloSwift",
+            path: "./Sources/PrintHelloSwift.xcframework")
     ]
 )
