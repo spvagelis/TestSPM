b6391328212ba9eed492ab69be4133ba1c6e711c
spvagelis <spirou.vagelis@gmail.com>
2024-12-17 17:22:42 +0200

    Added a new dependency CocoaLumberjack

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             name: "TestSPM",
             targets: ["TestSPM"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
          // Targets can depend on other targets in this package and products from dependencies.
